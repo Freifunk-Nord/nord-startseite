78c9182a183165acdfa7fe38f9ddb80acc8ada9f
ruben <ruben.git@r.z11.de>
2015-09-17 09:21:06 +0200

    Change into script dir before copying files

--- numnodes.sh
+++ numnodes.sh
@@ -6,6 +6,7 @@ JSON=/opt/ffmap-backend-neu/json/nodes.json
 echo -n "online nodes: "
 num="$(cat $JSON |jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==true)'|grep online|wc -l)"
 echo $num
+cd `dirname "$0"`
 sed 's/<NUM_NODES>/'$num'/g' numnodes.template > _site/numnodes.html
 
 #echo -n "offlilne nodes: "
