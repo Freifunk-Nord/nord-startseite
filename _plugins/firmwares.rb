f1ccccf37fb638f13377db22de8b6ed34bab5dc9
ruben <ruben.git@r.z11.de>
2015-09-17 08:33:03 +0200

    FIRMWARE_BASE typo

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -10,7 +10,7 @@ FIRMWARE_VERSION = '0.7.1'
 FIRMWARE_REGEX = Regexp.new('^' + FIRMWARE_PREFIX + '-' + FIRMWARE_VERSION + '-')
 #FIRMWARE_BASE = site.config['firmware']['base']
 #FIRMWARE_BASE = 'http://localhost/freifunk/firmware/ffki/0.7.1/'
-FIRMWARE_SITE = 'http://freifunk.discovibration.de/firmware/firmware-0.7.1/'
+FIRMWARE_BASE = 'http://freifunk.discovibration.de/firmware/firmware-0.7.1/'
 FIRMWARE_MIRROR = 'http://gluon.ffki.de/firmware/stable/'
 
 GROUPS = {
