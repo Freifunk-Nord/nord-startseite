15fdcab3532a0b019f8959fff6ad69bca52cb6ac
ruben <ruben.git@r.z11.de>
2015-09-17 08:37:16 +0200

    Add path to kieler nodes.json

--- numnodes.sh
+++ numnodes.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 # reads all online nodes and creates an HTML page from the template numnodes.template
-
-JSON=/var/www/freifunk/nodes.json
-# wget...
+JSON=/opt/ffmap-backend-neu/json/nodes.json
+#wget http://map.freifunk.in-kiel.de/json/nodes.json -qO $JSON
 
 echo -n "online nodes: "
 num="$(cat $JSON |jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==true)'|grep online|wc -l)"
 echo $num
-sed 's/<NUM_NODES>/'$num'/g' numnodes.template > _site/numnodes.html
+sed 's/<NUM_NODES>/'$num'/g' numnodes.template > numnodes.html
 
 #echo -n "offlilne nodes: "
 #cat $JSON |jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==false)'|grep online|wc -l
