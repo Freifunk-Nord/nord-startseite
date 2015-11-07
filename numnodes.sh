aa33ff313de610fee445e6a238fd8e8eacd5dd92
rubo77 <github@r.z11.de>
2015-11-07 05:13:56 +0100

    fill logfile with numnodes each minute

--- numnodes.sh
+++ numnodes.sh
@@ -4,12 +4,17 @@
 JSON=/opt/ffmap-backend-neu/json/alfred.158.json
 #wget http://map.freifunk.in-kiel.de/json/nodes.json -qO $JSON
 
+# turn on logging:
+logfile=/var/log/numnodes.log
+
 echo -n "online nodes: "
 #num="$(cat $JSON |jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==true)'|grep online|wc -l)"
 num="$(cat $JSON |grep hostname|wc -l)"
 echo $num
 cd `dirname "$0"`
 sed 's/<NUM_NODES>/'$num'/g' numnodes.template > build/numnodes.html
+
+echo "$(date) numnodes: $num">>$logfile
 
 #echo -n "offlilne nodes: "
 #cat $JSON |jq '.nodes[] | select(.flags.gateway==false) | select(.flags.online==false)'|grep online|wc -l
