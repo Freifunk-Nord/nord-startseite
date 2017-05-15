d727ea0d758f25cb81ec9712ffb89cc976130431
FFnord Service <admin@ffnord.net>
2017-05-16 01:11:59 +0200

    Firmwarelink nord.freifunk.net

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -9,7 +9,7 @@ FIRMWARE_VERSION = '2016.2.5'
 
 FIRMWARE_REGEX = Regexp.new('^' + FIRMWARE_PREFIX + '-' + FIRMWARE_VERSION + '-')
 #FIRMWARE_BASE = site.config['firmware']['base']
-FIRMWARE_BASE = 'https://freifunknord.de/firmware/stable/'
+FIRMWARE_BASE = 'https://nord.freifunk.net/firmware/stable/'
 FIRMWARE_MIRROR = 'https://github.com/Freifunk-Nord/nord-firmware-archiv/'
 
 GROUPS = {
