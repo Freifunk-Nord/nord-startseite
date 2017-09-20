223878136309061c21c86f03f5f9a8e56b7f64a0
FFnord Service <admin@ffnord.net>
2017-09-20 23:38:49 +0200

    firmware 2016.2.7 auf die download seite

--- _plugins/firmwares.rb
+++ _plugins/firmwares.rb
@@ -5,11 +5,12 @@ require 'pp'
 
 COMMUNITY_TLD = 'ffnord'
 FIRMWARE_PREFIX = 'gluon-' + COMMUNITY_TLD
-FIRMWARE_VERSION = '2016.2.5'
+FIRMWARE_VERSION = '2016.2.7'
 
 FIRMWARE_REGEX = Regexp.new('^' + FIRMWARE_PREFIX + '-' + FIRMWARE_VERSION + '-')
 #FIRMWARE_BASE = site.config['firmware']['base']
-FIRMWARE_BASE = 'https://nord.freifunk.net/firmware/stable/'
+#FIRMWARE_BASE = 'https://nord.freifunk.net/firmware/stable/'
+FIRMWARE_BASE = 'https://freifunk.in-kiel.de/nord-firmware/stable/'
 FIRMWARE_MIRROR = 'https://github.com/Freifunk-Nord/nord-firmware-archiv/'
 
 GROUPS = {
